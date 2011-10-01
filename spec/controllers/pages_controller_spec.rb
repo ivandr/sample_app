62a5619327300db7e59e9b2d24ec379b3e12a184
Ivan Pankov <ivandr.p@gmail.com>
2011-10-01 17:53:49 +0400

    Создали шиблон, и некоторые другие мелкие изменения, но пока не работает тест на title

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -7,12 +7,39 @@ describe PagesController do
       get 'home'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "My Home")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content =>
+                          "My Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "My About")
     end
   end
 
