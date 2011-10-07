a2a76cc4dc2ca4a1478d5ebefa2b2e3541dbb4f3
Ivan Pankov <ivandr.p@gmail.com>
2011-10-07 21:41:27 +0400

    Завершена глава 8. Была создана форма для регистрации пользователя, а так же ее валидация. Конечно же были созданы все необходимые тесты.

--- config/application.rb
+++ config/application.rb
@@ -6,13 +6,14 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "sprockets/railtie"
+require 'rails/all'
 # require "rails/test_unit/railtie"
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require *Rails.groups(:assets => %w(development test))
   # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
+  Bundler.require(:default, :assets, Rails.env)
 end
 
 module SampleApp
@@ -50,5 +51,8 @@ module SampleApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
   end
 end
