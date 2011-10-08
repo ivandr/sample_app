1ea73d9a38ea1d8339743f6c43c64b2225e2b2af
Ivan Pankov <ivandr.p@gmail.com>
2011-10-08 19:51:30 +0400

    Завершон урок 9. Создание контроллера Сессии. Сделана форма для входа. Обрабатывается сбой входа. Создано все для успешного входа, в том числе и проверка пароля. Создан выход. Создано уничтожение сессии. Изменен шаблон, появились кнопки входа и выхода.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -111,6 +111,11 @@ describe UsersController do
         response.should redirect_to(user_path(assigns(:user)))
       end
 
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+
       it "should have a welcome message" do
         post :create, :user => @attr
         flash[:success].should =~ /welcome to the sample app/i
