b6c656afe2097f8d8733de4df758b5d604865645
Ivan Pankov <ivandr.p@gmail.com>
2011-10-09 21:08:12 +0400

    Закончен урок 11. Созданы сообщения пользователей. А так же некоторые приблуды к ним, такие как удаление, контроль доступа и валидации. Тестирование сделано не все, осталось еще то что указано в управнениях к уроку.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111008175703) do
+ActiveRecord::Schema.define(:version => 20111009082835) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
