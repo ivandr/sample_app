294eaf1eabb29c71f2321a985d64294b17e1ac06
Ivan Pankov <ivandr.p@gmail.com>
2011-10-11 21:03:25 +0400

    Остановился на п.12.3. JS вызовы не работают надо доработать.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -21,7 +21,7 @@ describe PagesController do
 
       it "should have the right title" do
         response.should have_selector("title",
-                                      :content => "#{@base_title} | Home")
+                                      :content => "#{@base_title} Home")
       end
     end
 
@@ -29,7 +29,7 @@ describe PagesController do
 
       before(:each) do
         @user = test_sign_in(Factory(:user))
-        other_user = Factory(:user, :email => Factory.next(:email))
+        other_user = Factory(:user, :name => Factory.next(:name), :email => Factory.next(:email))
         other_user.follow!(@user)
       end
 
